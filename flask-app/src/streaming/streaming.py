f046233e6b81d5078d9240c0ae489c35be9da52d
ivyhsiao <119906196+ivyhsiao@users.noreply.github.com>
2022-12-13 22:54:02 -0500

    Update streaming.py

--- flask-app/src/streaming/streaming.py
+++ flask-app/src/streaming/streaming.py
@@ -24,15 +24,13 @@ def get_our_movies(streamingcompid):
     # get a cursor object from the database
     cursor = db.get_db().cursor()
 
-    query = '''
-        SELECT m.Title, m.description
-        FROM movie m JOIN starred_movies s ON m.movieid = s.movieid
-        JOIN streamed_movies sm ON sm.movieid = m.movieid
-        JOIN streamingcompany sc on sm.streamingcompanyid = sc.streamingcompanyid
-        WHERE m.streamingcompanyid = {0}'.format(streamingcompid)
-        ORDER BY m.numoflikes DESC
-        LIMIT 6;
-        '''
+    query = 'SELECT m.Title, m.description \
+        FROM Movie m JOIN starred_movies s ON m.movieid = s.movieid \
+        JOIN streamed_movies sm ON sm.movieid = m.movieid \
+        JOIN StreamingCompany sc on sm.StreamingCompanyId = sc.companyid \
+        WHERE sm.StreamingCompanyId = {0} \
+        ORDER BY m.numoflikes DESC \
+        LIMIT 6;'.format(streamingcompid)
 
     # use cursor to query the database for a list of streaming
     cursor.execute(query)
@@ -54,18 +52,18 @@ def get_our_movies(streamingcompid):
 
     return jsonify(json_data)
 
-# get the top 5 streaming from the database
-@streaming.route('/othersmovies', methods=['GET'])
+# get the top 6 popular streamed from other companies from the database
+@streaming.route('/othersmovies/<streamingcompid>', methods=['GET'])
 def get_others_movies(streamingcompid):
     cursor = db.get_db().cursor()
-    query = '''
-        SELECT m.Title, m.description, sc.companyname
-        FROM movie m JOIN starred_movies s ON m.movieid = s.movieid
-        JOIN streamed_movies sm ON sm.movieid = m.movieid
-        WHERE NOT sm.streamingcompanyid = {0}'.format(streamingcompid)
-        ORDER BY m.numoflikes DESC
-        LIMIT 6;
-    '''
+    query = 'SELECT m.Title, m.description, sc.companyname \
+        FROM Movie m JOIN starred_movies s ON m.movieid = s.movieid \
+        JOIN streamed_movies sm ON sm.movieid = m.movieid \
+        JOIN StreamingCompany sc on sm.StreamingCompanyid = sc.companyid \
+        WHERE NOT sm.streamingcompanyid = {0} \
+        ORDER BY m.numoflikes DESC \
+        LIMIT 6;'.format(streamingcompid)
+
     cursor.execute(query)
        # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
